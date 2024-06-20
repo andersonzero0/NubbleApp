f4e82af3d64dac55ab024663bafb8acc33d1be61
Anderson Viana <anderson11viana@gmail.com>
2024-06-19 21:41:28 -0300

    update

---
+++ src/components/Form/FormTextInput.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import {Controller, FieldValues, UseControllerProps} from 'react-hook-form';
+
+import {TextInput, TextInputProps} from '../TextInput/TextInput';
+
+export function FormTextInput<FormType extends FieldValues>({
+  control,
+  name,
+  rules,
+  ...textInputProps
+}: TextInputProps & UseControllerProps<FormType>) {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      rules={rules}
+      render={({field, fieldState}) => (
+        <TextInput
+          value={field.value}
+          onChangeText={field.onChange}
+          errorMessage={fieldState.error?.message}
+          {...textInputProps}
+        />
+      )}
+    />
+  );
+}
